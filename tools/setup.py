fd7b7bb4381310dde380e91430ec10b8f0eadf97
Thomas Patzke <thomas@patzke.org>
2017-12-09 08:57:51 +0100

    Fixed build
    Reference to main README

--- tools/setup.py
+++ tools/setup.py
@@ -8,7 +8,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, '../README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
