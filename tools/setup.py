4183b1b59ef21d093d09ba21ee30855e341a2cfc
Thomas Patzke <thomas@patzke.org>
2018-03-29 11:17:03 +0200

    Sigma tools release 0.3.3

--- tools/setup.py
+++ tools/setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sigmatools',
-    version='0.3.2',
+    version='0.3.3',
     description='Tools for the Generic Signature Format for SIEM Systems',
     long_description=long_description,
     url='https://github.com/Neo23x0/sigma',
