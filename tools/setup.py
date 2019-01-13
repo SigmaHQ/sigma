cc4b806b949107208d4907349b0f51afcca2a8e3
Thomas Patzke <thomas@patzke.org>
2019-01-14 00:26:03 +0100

    Sigma tools release 0.7.1

--- tools/setup.py
+++ tools/setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sigmatools',
-    version='0.6',
+    version='0.7.1',
     description='Tools for the Generic Signature Format for SIEM Systems',
     long_description=long_description,
     url='https://github.com/Neo23x0/sigma',
@@ -30,34 +30,38 @@ setup(
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Environment :: Console',
     ],
     keywords='security monitoring siem logging signatures elasticsearch splunk ids sysmon',
-    packages=['sigma'],
+    packages=['sigma', 'sigma.backends', 'sigma.config', 'sigma.parser'],
     python_requires='~=3.5',
-    install_requires=['PyYAML'],
+    install_requires=['PyYAML', 'pymisp'],
     extras_require={
         'test': ['coverage', 'yamllint'],
     },
     data_files=[
         ('etc/sigma', [
-            'config/arcsight.yml',
-            'config/elk-defaultindex-filebeat.yml',
-            'config/elk-defaultindex-logstash.yml',
+            'config/qualys.yml',
             'config/elk-defaultindex.yml',
-            'config/elk-linux.yml',
+            'config/arcsight.yml',
+            'config/sumologic.yml',
+            'config/netwitness.yml',
             'config/elk-windows.yml',
             'config/helk.yml',
+            'config/elk-defaultindex-logstash.yml',
+            'config/elk-linux.yml',
             'config/logpoint-windows-all.yml',
-            'config/qualys.yml',
-            'config/qradar.yml',
             'config/spark.yml',
-            'config/netwitness.yml',
+            'config/elk-winlogbeat.yml',
+            'config/elk-defaultindex-filebeat.yml',
             'config/splunk-windows-all.yml',
-            'config/sumologic.yml',
+            'config/qradar.yml',
+            'config/powershell-windows-all.yml',
         ])],
     scripts=[
         'sigmac',
-        'merge_sigma'
+        'merge_sigma',
+        'sigma2misp',
         ]
 )
