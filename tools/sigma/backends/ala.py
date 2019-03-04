c5796d7853f563a404ac3db0b7dbebfb0c82b8f7
tuckner <john.tuckner@optiv.com>
2019-03-04 10:49:50 -0600

    Added Azure Log Analytics backend

--- tools/sigma/backends/ala.py
+++ tools/sigma/backends/ala.py
@@ -1,3 +1,5 @@
+# Azure Log Analytics output backend for sigmac
+# John Tuckner (@tuckner)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
