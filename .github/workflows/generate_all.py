df77f1fdf0ea7fabafdd6d2b37f7f62b6bf36df4
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2019-11-15 10:54:43 -0500

    Fix paths.

--- .github/workflows/generate_all.py
+++ .github/workflows/generate_all.py
@@ -1,21 +1,21 @@
 import os
 
 def main():
-    os.system( 'mkdir ./sigma/lc-rules' )
+    os.system( 'mkdir ./lc-rules' )
     for d in ( 'windows/builtin',
                'windows/process_creation',
                'windows/sysmon', ):
         outDir = d.replace( '/', '_' )
         print( "Creating dir: %s" % ( outDir, ) )
-        os.system( 'mkdir ./sigma/lc-rules/%s' % ( outDir, ) )
-        thisPath = './sigma/rules/%s/' % ( d, )
+        os.system( 'mkdir ./lc-rules/%s' % ( outDir, ) )
+        thisPath = './rules/%s/' % ( d, )
         for f in os.listdir( thisPath ):
             thisFile = os.path.join( thisPath, f )
             if not os.path.isfile( thisFile ):
                 continue
             print( "Process rule %s" % ( thisFile, ) )
-            outFile = "./dr_rules/%s/%s" % ( outDir, f )
-            os.system( "python3 ./sigma/tools/sigmac -t limacharlie -c ./sigma/tools/config/limacharlie.yml %s > %s" % ( thisFile, outFile ) )
+            outFile = "./lc-rules/%s/%s" % ( outDir, f )
+            os.system( "python3 ./tools/sigmac -t limacharlie -c ./tools/config/limacharlie.yml %s > %s" % ( thisFile, outFile ) )
             if os.path.getsize( outFile ) == 0:
                 os.system( 'rm %s' % ( outFile, ) )
 
