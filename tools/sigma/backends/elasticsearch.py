ddfe16d21b0931c67f1d7f057740b9d387792e17
Florian Roth <venom14@gmail.com>
2020-01-31 07:24:21 +0100

    Adding change so that Travis cleans cache

--- tools/sigma/backends/elasticsearch.py
+++ tools/sigma/backends/elasticsearch.py
@@ -12,7 +12,7 @@
 # GNU Lesser General Public License for more details.
 
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 
 import json
 import re
