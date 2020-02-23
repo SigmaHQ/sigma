9cb395823cdc7f0467752f4caa7b2e3e07de09ef
Abhijit Khinvasara <abhikhinvasara@gmail.com>
2020-02-22 20:59:56 -0800

    Rework according to review comments.

--- tools/sigma/backends/logiq.py
+++ tools/sigma/backends/logiq.py
@@ -1,21 +1,31 @@
 import re
-from .base import BaseBackend
-from .mixins import QuoteCharMixin
+from .base import SingleTextQueryBackend
 import json
 
-class LogiqBackend(BaseBackend, QuoteCharMixin):
-    """Generates Perl compatible regular expressions and puts 'grep -P' around it"""
+class LogiqBackend(SingleTextQueryBackend):
+    """Converts Sigma rule into LOGIQ event rule api payload """
     identifier = "logiq"
-    active = True
     config_required = False
+    active = True
+    reEscape = re.compile('(")')
+    reClear = None
+    andToken = " && "
+    orToken = " || "
+    notToken = " !~ "
+    subExpression = "%s"
+    listExpression = "%s"
+    listSeparator = ", "
+    valueExpression = "message =~ \'%s\'"
+    keyExpression = "%s"
+    nullExpression = "%s"
+    notNullExpression = "!%s"
+    mapExpression = "(%s=%s)"
+    mapListsSpecialHandling = True
 
     reEscape = re.compile("([\\|()\[\]{}.^$+])")
 
     def generate(self, sigmaparser):
         """Method is called for each sigma rule and receives the parsed rule (SigmaParser)"""
-        print ("XXXXXX LogiqBackend definitions",sigmaparser.definitions)
-        print ("XXXXXX LogiqBackend values",sigmaparser.values)
-        print ("XXXXXX LogiqBackend config",sigmaparser.config)
 
         eventRule = dict()
         eventRule["name"] = sigmaparser.parsedyaml["title"]
@@ -24,10 +34,8 @@ class LogiqBackend(BaseBackend, QuoteCharMixin):
         eventRule["condition"] = sigmaparser.parsedyaml["detection"]
         eventRule["level"] = sigmaparser.parsedyaml["level"]
 
-
-        for key,value in eventRule.items():
-            print(key, ":", value)
-        print ("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        # for key,value in eventRule.items():
+        #     print(key, ":", value)
 
         for parsed in sigmaparser.condparsed:
             query = self.generateQuery(parsed)
@@ -42,37 +50,14 @@ class LogiqBackend(BaseBackend, QuoteCharMixin):
             if after is not None:
                 eventRule["condition"] += after
 
-            result = json.dumps(eventRule)
-
-            return result
-
-    def generateQuery(self, parsed):
-        # print("generateQuery: ", parsed)
-        return "%s" % self.generateNode(parsed.parsedSearch)
+            return json.dumps(eventRule)
 
     def cleanValue(self, val):
-        # val = super().cleanValue(val)
         if val[0] == '*':
             val = val.replace("*","/*")
-        
-        print("cleanValue: ", val)
+      
+        # print("cleanValue: ", val)
         return val
-
-    def generateORNode(self, node):
-        print("generateORNode: ", node)
-        return "%s" % " || ".join([self.generateNode(val) for val in node])
-
-    def generateANDNode(self, node):
-        print("generateORNode: ", node)
-        return "%s" % " && ".join([self.generateNode(val) for val in node])
-        
-    def generateNOTNode(self, node):
-        print("generateNOTNode: ", node)        
-        return "%s" % self.generateNode(node.item)
-
-    def generateSubexpressionNode(self, node):
-        # print("generateSubexpressionNode: ", node)        
-        return "%s" % self.generateNode(node.items)
 
     def generateListNode(self, node):
         # print("generateListNode: ", node)
@@ -81,18 +66,9 @@ class LogiqBackend(BaseBackend, QuoteCharMixin):
         return self.generateORNode(node)
 
     def generateMapItemNode(self, node):
-        print("generateMapItemNode: ", node)
+        # print("generateMapItemNode: ", node)
         key, value = node
         if value is None:
             return self.generateNULLValueNode(node)
         else:
             return self.generateNode(value)
-
-    def generateValueNode(self, node):
-        print("generateValueNode: ", node)
-        return "message =~ '" + self.cleanValue(str(node)).strip() + "'"
-
-    def generateNULLValueNode(self, node):
-        print("generateNULLValueNode: ", node)
-        key, value = node
-        return "%s" % key
