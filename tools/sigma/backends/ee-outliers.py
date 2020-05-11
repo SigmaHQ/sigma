37b08543ac6b63b4c457b0ef87f8afcc91883cf5
Remco Hofman <rhofman@nviso.be>
2020-05-11 11:47:56 +0200

    Updated author reference in license

--- tools/sigma/backends/ee-outliers.py
+++ tools/sigma/backends/ee-outliers.py
@@ -1,5 +1,5 @@
 # ee-outliers backend for sigmac
-# NVISO (@NVISOsecurity)
+# NVISO (@NVISO_Labs)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
