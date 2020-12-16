544012897954f0716ed770e566fb85042426f842
joker2013 <ivan.joker2009@mail.ru>
2020-12-16 12:18:24 +0300

    123

--- tools/pull-sigma.sh
+++ tools/pull-sigma.sh
@@ -19,54 +19,54 @@ echo "------------------------------------------------"
 echo " "
 rule_counter=0
 ESALERT_HOME="../elastalert_rules"
-# Windows rules
-for  rule_category in ../rules/windows/* ; do
-    echo " "
-    echo "Working on Folder: $rule_category:"
-    echo "-------------------------------------------------------------"
-    if [ "$rule_category" == ../rules/windows/process_creation ]; then
-        for rule in $rule_category/* ; do
-            if [ $rule != ../rules/windows/process_creation/win_mal_adwind.yml ];
-            then
-                if SIGMAremoveNearRules "$rule"; then
-                    continue
-                else
-                    echo "[+++] Processing Windows process creation rule: $rule .."
-                    ./sigmac -t elastalert -c config/generic/sysmon.yml -c config/wazuh.yml -o ${ESALERT_HOME}/sigma_$(basename $rule) "$rule"
-                    # Give unique rule name for sysmon
-                    sed -i '' 's/^name: /name: Sysmon_/' ${ESALERT_HOME}/sigma_sysmon_$(basename $rule)
-                    ./sigmac -t elastalert -c config/generic/windows-audit.yml -c config/wazuh.yml -o ${ESALERT_HOME}/sigma_sysmon_$(basename $rule) "$rule"
-                    #ls -la "${ESALERT_HOME}"/rules/sigma_sysmon_"$(basename "${rule}")"
-                    rule_counter=$[$rule_counter +1]
-                fi
-            fi
-        done
-    else
-        for rule in $rule_category/* ; do
-            if SIGMAremoveNearRules "$rule"; then
-                continue
-            else
-                echo "[+++] Processing additional Windows rule: $rule .."
-                ./sigmac -t elastalert -c config/wazuh.yml -o ${ESALERT_HOME}/sigma_$(basename $rule) "$rule"rules/sigma_$(basename $rule) $rule
-                sed -i '' "s/^name: .*/name: sigma_$(basename -s .yml $rule)/" ${ESALERT_HOME}/sigma_$(basename $rule)
-                rule_counter=$[$rule_counter +1]
-            fi
-        done
-    fi
-done
+# # Windows rules
+# for  rule_category in ../rules/windows/* ; do
+#     echo " "
+#     echo "Working on Folder: $rule_category:"
+#     echo "-------------------------------------------------------------"
+#     if [ "$rule_category" == ../rules/windows/process_creation ]; then
+#         for rule in $rule_category/* ; do
+#             if [ $rule != ../rules/windows/process_creation/win_mal_adwind.yml ];
+#             then
+#                 if SIGMAremoveNearRules "$rule"; then
+#                     continue
+#                 else
+#                     echo "[+++] Processing Windows process creation rule: $rule .."
+#                     ./sigmac -t elastalert -c config/generic/sysmon.yml -c config/wazuh.yml -o "${ESALERT_HOME}"/sigma_"$(basename "$rule")" "$rule"
+#                     # Give unique rule name for sysmon
+#                     sed -i '' 's/^name: /name: Sysmon_/' "${ESALERT_HOME}"/sigma_sysmon_"$(basename "$rule")"
+#                     ./sigmac -t elastalert -c config/generic/windows-audit.yml -c config/wazuh.yml -o ${ESALERT_HOME}/sigma_sysmon_"$(basename "$rule")" "$rule"
+#                     #ls -la "${ESALERT_HOME}"/rules/sigma_sysmon_"$(basename "${rule}")"
+#                     rule_counter=$[$rule_counter +1]
+#                 fi
+#             fi
+#         done
+#     else
+#         for rule in $rule_category/* ; do
+#             if SIGMAremoveNearRules "$rule"; then
+#                 continue
+#             else
+#                 echo "[+++] Processing additional Windows rule: $rule .."
+#                 ./sigmac -t elastalert -c config/wazuh.yml -o "${ESALERT_HOME}"/sigma_"$(basename "$rule")" "$rule"rules/sigma_"$(basename "$rule")" $rule
+#                 sed -i '' "s/^name: .*/name: sigma_"$(basename -s .yml "$rule")"/" "${ESALERT_HOME}"/sigma_"$(basename "$rule")"
+#                 rule_counter=$[$rule_counter +1]
+#             fi
+#         done
+#     fi
+# done
 # Apt rules
 echo " "
 echo "Working on Folder: apt:"
 echo "-------------------------------------------------------------"
-for rule in rules/apt/* ; do
+for rule in ../rules/apt/* ; do
     if SIGMAremoveNearRules "$rule"; then
         continue
     else
         echo "[+++] Processing apt rule: $rule .."
-        ./sigmac -t elastalert -c config/generic/sysmon.yml -c config/wazuh.yml -o ${ESALERT_HOME}/sigma_apt_$(basename $rule) "$rule"
+        ./sigmac -t elastalert -c config/generic/sysmon.yml -c config/wazuh.yml -o "${ESALERT_HOME}"/sigma_apt_"$(basename "$rule")" "$rule"
         # Give unique rule name for sysmon
-        sed -i '' 's/^name: /name: Sysmon_/' ${ESALERT_HOME}/sigma_sysmon_apt_$(basename $rule)
-        ./sigmac -t elastalert -c config/generic/windows-audit.yml -c config/wazuh.yml -o ${ESALERT_HOME}/sigma_sysmon_apt_$(basename $rule) "$rule"
+        sed -i '' 's/^name: /name: Sysmon_/' "${ESALERT_HOME}"/sigma_sysmon_apt_"$(basename "$rule")"
+        ./sigmac -t elastalert -c config/generic/windows-audit.yml -c config/wazuh.yml -o "${ESALERT_HOME}"/sigma_sysmon_apt_"$(basename "$rule")" "$rule"
         rule_counter=$[$rule_counter +1]
     fi
 done
