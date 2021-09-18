88a59be69c2e8fbd00ef26762ee909b1ff012f04
frack113 <magicfrancois@gmail.com>
2021-09-18 18:13:16 +0200

    Add options and return error code

--- tools/sigma/sigma_configurations_check.py
+++ tools/sigma/sigma_configurations_check.py
@@ -18,39 +18,72 @@
 import sigma.backends.discovery as backends
 import ruamel.yaml
 from pathlib import Path
-
+from argparse import ArgumentParser
+import sys
+import csv
+    
 def main():
+    argparser = ArgumentParser(description="A simple Sigma Configurations checker")
+    argparser.add_argument("--verify", "-V", action="store_true", help="Verify if configuration file have valid backend name")
+    argparser.add_argument("--sumary", "-s", action="store_true", help="Give some information.")
+    argparser.add_argument("--error", "-e", action="store_true", help="Exit with error code 10 on verification failures.")
+    argparser.add_argument("--output", "-o", default=None, help="Output csv file")
+    args = argparser.parse_args()
+    
+    passed = True
+    
     list_backend =[]
     for backend in sorted(backends.getBackendList(), key=lambda backend: backend.identifier):
         list_backend.append(backend.identifier)
 
-    print(f"Valid backend name are {list_backend}")
-    print('result tab:')
-    print('|Name file|Backend|check')
-    print('|---|---|---')
+    if args.sumary: 
+        print(f"Backend found :\n{list_backend}\n")
 
-    valid = 0
-    empty = 0
-    faulty = 0
-    yml_files =Path('config/').glob("*.yml")
-    for yml in yml_files:
-       with yml.open("r",encoding="UTF-8") as f:
-           data = ruamel.yaml.load(f,Loader=ruamel.yaml.RoundTripLoader)
-           if 'backends' in data:
-               for backend in data['backends']:
-                   if backend in list_backend:
-                       print(f"|{yml.name:45} | {backend:30} | OK ")
-                       valid += 1
-                   else:
-                       print(f"|{yml.name:45} | {backend:30} | NOK")
-                       faulty += 1
-           else:
-              msg = "no backend set"
-              print(f"|{yml.name:45} | {msg:30} | -  ")
-              empty += 1
+    if args.verify:
+        csv_lst = []
+        valid = 0
+        empty = 0
+        faulty = 0
+        yml_files =Path('config/').glob("*.yml")
+        for yml in yml_files:
+            print(f"Check configurations file : {yml.name}")
+            with yml.open("r",encoding="UTF-8") as f:
+                data = ruamel.yaml.load(f,Loader=ruamel.yaml.RoundTripLoader)
+                if 'backends' in data:
+                    for backend in data['backends']:
+                        if backend in list_backend:
+                            csv_lst.append([yml.name,backend,'OK'])
+                            valid += 1
+                        else:
+                            csv_lst.append([yml.name,backend,'NOK'])
+                            faulty += 1
+                            passed = False
+                else:
+                    csv_lst.append([yml.name,"no backends section",'-'])
+                    empty += 1
+                    #passed = False 
+                    #Should not be but not sure
+            
+        if args.sumary: 
+            print('-------')
+            print('Summary')
+            print(f'Valid backend name: {valid}\nInvalid backend name: {faulty}\nFile with no Backend: {empty}')
+            print('-------')
 
-    print('Summary')
-    print(f'Valid : {valid}  Invalid : {faulty} No Backend : {empty}')
+        if args.output:
+            with open(args.output, 'w', newline='') as csvfile:
+                spamwriter = csv.writer(csvfile, delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                spamwriter.writerow(['Configurations Name','Backend Name','Result'])
+                for row in csv_lst:
+                    spamwriter.writerow(row)
+                
+        if not passed:
+            print("**************************************")
+            print("Some Configurations file are not valid")
+            print("**************************************")
+            if args.error:
+                exit(10)
+                
 
 if __name__ == "__main__":
     main()
