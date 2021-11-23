81d375600850352421c6e19bda84c8637057162e
Julien Doutre <julien.doutre@datadoghq.com>
2021-11-23 17:51:03 +0100

    Simple rules support

--- tools/sigma/backends/datadog.py
+++ tools/sigma/backends/datadog.py
@@ -2,6 +2,7 @@ from sigma.backends.base import SingleTextQueryBackend
 
 
 class DatadogBackend(SingleTextQueryBackend):
+    """Converts Sigma rule into Datadog log search queries."""
     identifier = "datadog"  # TODO: more specific?
     active = True
     config_required = False
@@ -10,4 +11,20 @@ class DatadogBackend(SingleTextQueryBackend):
     orToken = " OR "
     notToken = "-"
     subExpression = "(%s)"
+    listExpression = "(%s)"
+    listSeparator = " OR "
+    valueExpression = "%s"
     mapExpression = "%s:%s"
+    nullExpression = ""
+
+    service = None
+
+    def generate(self, sigmaparser):
+        self.service = sigmaparser.parsedyaml['logsource'].get('service', "")
+        return super().generate(sigmaparser)
+
+    def generateQuery(self, parsed):
+        return self.generateANDNode([
+            self.generateMapItemNode(["service", self.service]),
+            self.generateNode(parsed.parsedSearch),
+        ])
