beab887ad1b57eec88a73fe94387880d4d319662
Julien Doutre <julien.doutre@datadoghq.com>
2021-11-29 15:11:29 +0100

    Escape queries

--- tools/sigma/backends/datadog.py
+++ tools/sigma/backends/datadog.py
@@ -1,3 +1,5 @@
+import re
+
 from sigma.backends.base import SingleTextQueryBackend
 
 
@@ -14,9 +16,15 @@ class DatadogBackend(SingleTextQueryBackend):
     subExpression = "(%s)"
     listExpression = "(%s)"
     listSeparator = " OR "
-    valueExpression = "%s"  # TODO: escape string containing special chars
+    valueExpression = "%s"
     mapExpression = "%s:%s"
     nullExpression = ""
+
+    # The escaped characters list comes from https://docs.datadoghq.com/logs/explorer/search_syntax/#escaping-of-special-characters.
+    specialCharactersRegexp = re.compile(
+        '([\+\-\=\&\|\>\<\!\(\)\{\}\[\]\^"\~\?\:\\\/]+)'
+    )
+    whitespacesRegexp = re.compile(r"(\s+)")
 
     def __init__(self, sigmaconfig, backend_options):
         if "index" in backend_options:
@@ -34,11 +42,19 @@ class DatadogBackend(SingleTextQueryBackend):
         nodes = []
 
         if hasattr(self, "dd_index"):
-            nodes.append(self.generateMapItemNode(["index", self.dd_index]))
+            nodes.append(("index", self.dd_index))
 
         if hasattr(self, "dd_service"):
-            nodes.append(self.generateMapItemNode(["service", self.dd_service]))
+            nodes.append(("service", self.dd_service))
 
-        nodes.append(self.generateNode(parsed.parsedSearch))
+        nodes.append(parsed.parsedSearch)
 
         return self.generateANDNode(nodes)
+
+    def cleanValue(self, val):
+        if type(val) == int:
+            return val
+        else:
+            return self.whitespacesRegexp.sub(
+                "?", self.specialCharactersRegexp.sub("\\\\\g<1>", val)
+            )
