477c9cf0486a0324ebe98b7c3ae776c1d170d097
Julien Doutre <julien.doutre@datadoghq.com>
2021-12-15 16:26:31 +0100

    Refactor tests basic rule

--- tools/tests/test_backend_datadog.py
+++ tools/tests/test_backend_datadog.py
@@ -28,51 +28,6 @@ from sigma.backends.datadog import DatadogLogsBackend
 class TestDatadogLogsBackend(unittest.TestCase):
     """Test cases for the Datadog Logs backend."""
 
-    def setUp(self):
-        self.basic_rule = {
-            "detection": {"selection": {"attribute": "test"}, "condition": "selection"}
-        }
-
-    def test_attribute(self):
-        query = self.generate_query(self.basic_rule)
-        expected_query = "@attribute:test"
-        self.assertEqual(query, expected_query)
-
-    def test_facets_backend_option(self):
-        query = self.generate_query(
-            self.basic_rule, backend_options={"index": "test_index"}
-        )
-        expected_query = "index:test_index AND @attribute:test"
-        self.assertEqual(query, expected_query)
-
-    def test_facets_config(self):
-        self.basic_rule["detection"]["selection"]["test-facet"] = "myfacet"
-        query = self.generate_query(self.basic_rule, config={"facets": ["test-facet"]})
-        expected_query = "@attribute:test AND test-facet:myfacet"
-        self.assertEqual(query, expected_query)
-
-    def test_special_characters_escape(self):
-        self.basic_rule["detection"]["selection"][
-            "regex-attribute"
-        ] = "anything?inbetween"
-        query = self.generate_query(self.basic_rule)
-        expected_query = "@attribute:test AND @regex-attribute:anything\\?inbetween"
-        self.assertEqual(query, expected_query)
-
-    def test_space_escape(self):
-        self.basic_rule["detection"]["selection"]["space-attribute"] = "with space"
-        query = self.generate_query(self.basic_rule)
-        expected_query = "@attribute:test AND @space-attribute:with?space"
-        self.assertEqual(query, expected_query)
-
-    def test_space_escape(self):
-        query = self.generate_query(
-            self.basic_rule,
-            fieldmappings={"attribute": FieldMapping("attribute", "another_attribute")},
-        )
-        expected_query = "@another_attribute:test"
-        self.assertEqual(query, expected_query)
-
     def generate_query(
         self, rule, backend_options=dict(), config=dict(), fieldmappings=dict()
     ):
@@ -83,6 +38,70 @@ class TestDatadogLogsBackend(unittest.TestCase):
         parser = SigmaParser(rule, cfg)
 
         return backend.generate(parser)
+
+    def generate_basic_rule(self):
+        return {
+            "detection": {"selection": {"attribute": "test"}, "condition": "selection"}
+        }
+
+    def test_attribute(self):
+        query = self.generate_query(self.generate_basic_rule())
+        expected_query = "@attribute:test"
+        self.assertEqual(query, expected_query)
+
+    def test_facets_backend_option(self):
+        query = self.generate_query(
+            self.generate_basic_rule(), backend_options={"index": "test_index"}
+        )
+        expected_query = "index:test_index AND @attribute:test"
+        self.assertEqual(query, expected_query)
+
+    def test_facets_config(self):
+        rule = {
+            "detection": {
+                "selection": {"attribute": "test", "test-facet": "myfacet"},
+                "condition": "selection",
+            }
+        }
+        query = self.generate_query(rule, config={"facets": ["test-facet"]})
+        expected_query = "@attribute:test AND test-facet:myfacet"
+        self.assertEqual(query, expected_query)
+
+    def test_special_characters_escape(self):
+        rule = {
+            "detection": {
+                "selection": {
+                    "attribute": "test",
+                    "regex-attribute": "anything?inbetween",
+                },
+                "condition": "selection",
+            }
+        }
+        query = self.generate_query(rule)
+        expected_query = "@attribute:test AND @regex-attribute:anything\\?inbetween"
+        self.assertEqual(query, expected_query)
+
+    def test_space_escape(self):
+        rule = {
+            "detection": {
+                "selection": {
+                    "attribute": "test",
+                    "space-attribute": "with space",
+                },
+                "condition": "selection",
+            }
+        }
+        query = self.generate_query(rule)
+        expected_query = "@attribute:test AND @space-attribute:with?space"
+        self.assertEqual(query, expected_query)
+
+    def test_space_escape(self):
+        query = self.generate_query(
+            self.generate_basic_rule(),
+            fieldmappings={"attribute": FieldMapping("attribute", "another_attribute")},
+        )
+        expected_query = "@another_attribute:test"
+        self.assertEqual(query, expected_query)
 
     def test_all_sigma_rules(self):
         """Test the Datadog backend over all the Sigma rules in the repository."""
