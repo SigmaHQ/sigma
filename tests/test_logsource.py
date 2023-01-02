15798527e2a7780239bb7ca2b525727ba4cacd53
Nasreddine Bencherchali <8741929+nasbench@users.noreply.github.com>
2023-01-02 21:33:15 +0100

    fix: typo in message

--- tests/test_logsource.py
+++ tests/test_logsource.py
@@ -161,7 +161,7 @@ class TestRules(unittest.TestCase):
                         for field in self.get_detection_field(detection):
                             if not check_category("empty",full_logsource['category'],field):
                                 print(
-                                    Fore.RED + "Rule {} has the invalid field <{}> forcategory <{}>".format(file, field,full_logsource['category']))
+                                    Fore.RED + "Rule {} has the invalid field <{}> for category <{}>".format(file, field,full_logsource['category']))
                                 files_with_fieldname_issues.append(file)                        
 
         self.assertEqual(files_with_fieldname_issues, [], Fore.RED +
