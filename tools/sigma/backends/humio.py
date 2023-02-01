7c38a5c4966d8088576a324397c1c4ab037df17c
Nasreddine Bencherchali <8741929+nasbench@users.noreply.github.com>
2023-02-01 11:14:59 +0100

    chore: add nextron authors tag

--- tools/sigma/backends/humio.py
+++ tools/sigma/backends/humio.py
@@ -1,5 +1,5 @@
 # Output backends for sigmac
-# Copyright 2016-2018 Thomas Patzke, Florian Roth, Roey
+# Copyright 2016-2018 Thomas Patzke, Florian Roth (Nextron Systems), Roey
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
