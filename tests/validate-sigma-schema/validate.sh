aa20859aaa04f169dde73f51060b7b6b48cdfec7
Mostafa Moradian <mostafamoradian0@gmail.com>
2024-02-20 13:02:29 +0100

    Remove comment

--- tests/validate-sigma-schema/validate.sh
+++ tests/validate-sigma-schema/validate.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 echo "Validating Sigma rules against sigma-schema.json"
-# SIGMA_REPO_PATH is a variable that contains the path to the Sigma repository,
-# DO NOT USE THIS VARIABLE IN YOUR CODE, IT IS FOR TESTING PURPOSES ONLY IN SIGMA REPOSITORY.
 check-jsonschema --schemafile $(python tests/validate-sigma-schema/validate.py schema) $(python tests/validate-sigma-schema/validate.py rules)
