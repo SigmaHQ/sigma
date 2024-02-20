26ceab4d96579a7fd5c770df68ebc8fa5f19e656
Mostafa Moradian <mostafamoradian0@gmail.com>
2024-02-20 11:41:20 +0100

    Fix issues with path and env-vars

--- tests/validate-sigma-schema/validate.sh
+++ tests/validate-sigma-schema/validate.sh
@@ -3,4 +3,4 @@
 echo "Validating Sigma rules against sigma-schema.json"
 # SIGMA_REPO_PATH is a variable that contains the path to the Sigma repository,
 # DO NOT USE THIS VARIABLE IN YOUR CODE, IT IS FOR TESTING PURPOSES ONLY IN SIGMA REPOSITORY.
-check-jsonschema --schemafile $(python ./${SIGMA_REPO_PATH}validate.py schema) $(python ./${SIGMA_REPO_PATH}validate.py rules)
+check-jsonschema --schemafile $(python tests/validate-sigma-schema/validate.py schema) $(python tests/validate-sigma-schema/validate.py rules)
