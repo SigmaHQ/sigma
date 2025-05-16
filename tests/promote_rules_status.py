c2f6f6cdd8ee683c31a29aed9fbbe3514d8f5501
phantinuss <79651203+phantinuss@users.noreply.github.com>
2025-05-16 12:37:39 +0200

    chore: black formatter and readability

--- tests/promote_rules_status.py
+++ tests/promote_rules_status.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 from datetime import datetime
 from functools import reduce
-from json import dumps
-from os import path, sep
 from sigma.collection import SigmaCollection
 from typing import Iterator
+import json
+import os
 
 PATH_TO_RULES = [
     "rules",
@@ -20,14 +20,20 @@ NB_DAYS = 300
 def is_experimental_and_older_than_ref(sigmaHQrule: "sigma.rule.SigmaRule") -> bool:
     last_update = sigmaHQrule.modified if sigmaHQrule.modified else sigmaHQrule.date
 
-    return str(sigmaHQrule.status) == "experimental" and (datetime.now().date() - last_update).days >= NB_DAYS
+    return (
+        str(sigmaHQrule.status) == "experimental"
+        and (datetime.now().date() - last_update).days >= NB_DAYS
+    )
 
 
 def get_rules_to_promote() -> Iterator[str]:
     rule_paths = SigmaCollection.resolve_paths(PATH_TO_RULES)
     rule_collection = SigmaCollection.load_ruleset(rule_paths, collect_errors=True)
 
-    return (rule.source.path for rule in filter(is_experimental_and_older_than_ref, rule_collection))
+    return (
+        rule.source.path
+        for rule in filter(is_experimental_and_older_than_ref, rule_collection)
+    )
 
 
 def promote_rule(rule: str) -> str:
@@ -39,15 +45,17 @@ def promote_rule(rule: str) -> str:
 
     return rule
 
+
 def summarize_promotion(summary: dict, rule: str) -> dict:
-    key, *values = rule.split(sep)
-    value = sep.join(values)
+    key, *values = rule.split(os.sep)
+    value = os.sep.join(values)
     summary[key].append(value)
 
     return summary
+
 
 if __name__ == "__main__":
     rules = (str(promote_rule(rule)) for rule in get_rules_to_promote())
     promotion = reduce(summarize_promotion, rules, defaultdict(list))
 
-    print(dumps(promotion))
+    print(json.dumps(promotion))
