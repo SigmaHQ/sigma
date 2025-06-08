3499d59031726384b63d5ef1f235ba9f992ec49c
Ariel Otilibili <otilibil@eurecom.fr>
2025-06-08 19:05:56 +0200

    tests: refactor deprecated_rules.py
    Scaffold for an upcoming commit.
    
    Signed-off-by: Ariel Otilibili <otilibil@eurecom.fr>

--- tests/deprecated_rules.py
+++ tests/deprecated_rules.py
@@ -9,39 +9,47 @@ Run using the command
 
 from sigma.collection import SigmaCollection
 from sigma.rule import SigmaStatus,SigmaLevel
+
+import argparse
 import datetime
 import csv
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f',  '--format', choices=['csv'], default='csv')
+args = parser.parse_args()
 
 path_to_rules = [
     "deprecated",
 ]
-name_csv_export = "./deprecated/deprecated.csv"
 
+def get_level(rule):
+    return rule.level if rule.status else SigmaLevel.MEDIUM
+
+def get_modified_time(rule):
+    return rule.modified if rule.modified else datetime.date.today()
+
+def format_rule(rule):
+    return {
+        "id": str(rule.id),
+        "title": rule.title,
+        "date": str(rule.date),
+        "modified": str(get_modified_time(rule)),
+        "level": str(get_level(rule))
+    }
 
 def save_csv(rules):
+    is_rule_deprecated = lambda rule: rule.status is SigmaStatus.DEPRECATED
+    name_csv_export = f"./deprecated/deprecated.{args.format}"
+
+    raw_info = map(format_rule, filter(is_rule_deprecated, rules))
+    sort_info = sorted(raw_info, key=lambda d: d['modified'])
+
     with open(name_csv_export, encoding="UTF-8", mode="w", newline="") as csv_file:
-        fieldnames = ["id", "title", "date", "modified","level"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-
-        raw_info = []
-        for rule in rules:
-            if rule.status is SigmaStatus.DEPRECATED:
-                modified = rule.modified if rule.modified else datetime.date.today()
-                level = rule.level if rule.status else SigmaLevel.MEDIUM
-                raw_info.append(
-                    {
-                        "id": rule.id,
-                        "title": rule.title,
-                        "date": rule.date,
-                        "modified": modified,
-                        "level": level
-                    }
-                )
-        
-        sort_info = sorted(raw_info, key=lambda d: d['modified'])
-        writer.writerows(sort_info)
-
+        if args.format == "csv":
+            fieldnames = ["id", "title", "date", "modified","level"]
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(sort_info)
 
 if __name__ == "__main__":
 
